b34f83903bd44cdaa109cfe16c28e7962955e542
Jacob Knapp <jacak@sbcglobal.net>
2013-04-07 22:49:49 -0400

    Add winCol and winRow

--- engine.lisp
+++ engine.lisp
@@ -12,9 +12,41 @@
   (dotimes (i (length board))
     (print-items (nth i board))))
 
-	
-	
-	
+(defun winHor (board win)
+	(let ((allwin nil))
+		(dotimes (col (length board) allwin)
+			(dotimes (row (- (length (car board)) (- win 1)))
+				(let ((start  row))
+					(if (< (+ start win) (- (length (car board)) 1))
+						(let ((winner ( nth row (nth col))))
+							(dotimes (winRow win winner)
+								(if (eq (nth (+ winRow start) (nth col)) winner)
+ 									(set winner (nth (+ winRow start) (nth col)))
+									(set winner nil)
+								)
+							)
+						(if (/= winner nil)
+							(set allwin winner)))))))))
+							
+(defun winCol (board win)
+	(let ((allwin nil))
+		(dotimes (row (length (car board) allwin))
+			(dotimes (col (- (length board) ( - win 1)))
+				(let ((start cols))
+					(if (< (+ start win) (- (length board) 1))
+						(let ((winner (nth row (nth col))))
+							(dotimes (winCol win winner)
+								(if (eq (nth row (nth (+ winCol start))) winner)
+									(set winner (nth row (nth (+ winCol start))))
+									(set winner nil)
+								)
+							)
+						(if (/= winner nil)
+							(set allwin winner)))))))))
+		
+		
+		
+
 (defun place-tok (board col player)
   (if (not(null (nth col (first (last board)))))
 	  (place-tok (butlast board) col player ) (setf (nth col (first (last board))) player)))
